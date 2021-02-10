fd29d4800dbd6f885714f2dd0faa3558d91db934
Ashley Brown <1thing.ashleyb@gmail.com>
2021-02-10 14:59:00 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        private int GameCount = 0;
+        private int Wins = 0;
+        private int Losses = 0;
+        static void Main(string[] args)
+        {
+            GetUserInput();
+            Console.WriteLine($"The computer played {GenerateRandomOutput()}");
+        }
+
+        public enum Option
+        {
+            rock,
+            paper,
+            scissors
+        }
+
+        static Option GetUserInput() 
+        {
+            Console.WriteLine("Enter rock, paper, or scissors to play");
+            string input = System.Console.ReadLine().ToLower();
+
+            Enum.TryParse(input, out Option option);
+            return option;
+        }
+
+        static Option GenerateRandomOutput() 
+        {
+            Random random = new Random();
+            int randomInt = random.Next(1, 3);
+            string stringOption = null;
+            if (randomInt == 0) 
+            {
+                stringOption = "rock";
+            } 
+            else if (randomInt == 1) 
+            {
+                stringOption = "paper";
+            } 
+            else 
+            {
+                stringOption = "scissors";
+            }
+            Enum.TryParse(stringOption, out Option option);
+
+            return option;
+        }
+
+        static void WhoWins() 
+        {
+            
+        }
+    }
+}
